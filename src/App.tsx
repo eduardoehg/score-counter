73841067f89a62f42c9d086131f582aafb032f22
eduardoehg <eduardoehg@outlook.com>
2025-12-23 11:53:12 -0300

    feat/refactor: atualização nas imagens / ajuste nos botões

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,43 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { KeepAwake } from "@capacitor-community/keep-awake";
+import { App as CapacitorApp } from "@capacitor/app";
 import Home from "./pages/Home";
 import Capote from "./pages/Capote";
 import Truco from "./pages/Truco";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+
+const BackButtonHandler = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleBackButton = async () => {
+      CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+        if (location.pathname !== "/") {
+          // If not on home, go to home
+          navigate("/");
+        } else {
+          // If on home, exit app
+          CapacitorApp.exitApp();
+        }
+      });
+    };
+
+    handleBackButton();
+
+    return () => {
+      CapacitorApp.removeAllListeners();
+    };
+  }, [navigate, location]);
+
+  return null;
+};
 
 const App = () => {
   useEffect(() => {
@@ -32,6 +60,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <BackButtonHandler />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/capote" element={<Capote />} />
@@ -44,5 +73,6 @@ const App = () => {
     </QueryClientProvider>
   );
 };
+
 
 export default App;
