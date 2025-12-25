0d5e9ea6bddfcc6a878384216f64719dda88beb6
eduardoehg <eduardoehg@outlook.com>
2025-12-24 22:33:24 -0300

    refactor: ajustes para as imagens do app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.eduardo.score',
-  appName: 'score-counter',
+  appName: 'Score Counter',
   webDir: 'dist'
 };
 
